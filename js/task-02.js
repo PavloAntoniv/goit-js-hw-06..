c24a0abc88ad4c455774d1f10617e68c1419358e
Pavlo Antoniv <pavlivavli@gmail.com>
2023-03-16 14:46:06 +0100

    Initial commit

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ingredientList = document.querySelector('#ingredients');
+
+const ingredientElements = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+ingredientList.append(...ingredientElements);
+
+console.log(ingredientList.innerHTML);
